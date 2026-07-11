e6e5e0ecd85f53bc9f261173cd5a6fcbdb5d7bd1
takeokunn <bararararatty@gmail.com>
2026-07-11 20:27:18 +0900

    test(rename-local-function): cover macro accessors

--- src/application/usecase/rename/tests/local_function/accessors.rs
+++ src/application/usecase/rename/tests/local_function/accessors.rs
@@ -3,27 +3,27 @@ use super::*;
 #[test]
 fn plans_flet_rename_without_touching_global_function_cell_accessors() {
     assert_local_function_rename!(
-        input: "(flet ((foo (x) x)) (symbol-function 'foo) (fdefinition 'foo) (foo 1) foo)\n",
+        input: "(flet ((foo (x) x)) (macro-function 'foo) (compiler-macro-function 'foo) (symbol-function 'foo) (fdefinition 'foo) (foo 1) foo)\n",
         dialect: Dialect::CommonLisp,
         from: "foo",
         to: "bar",
         definitions: 1,
         calls: 1,
         changed: true,
-        rewritten_contains: ["(flet ((bar (x) x)) (symbol-function 'foo) (fdefinition 'foo) (bar 1) foo)"]
+        rewritten_contains: ["(flet ((bar (x) x)) (macro-function 'foo) (compiler-macro-function 'foo) (symbol-function 'foo) (fdefinition 'foo) (bar 1) foo)"]
     );
 }
 
 #[test]
 fn plans_labels_rename_without_touching_global_function_cell_accessors() {
     assert_local_function_rename!(
-        input: "(labels ((foo (x) (foo x))) (symbol-function 'foo) (fdefinition 'foo) (foo 1) foo)\n",
+        input: "(labels ((foo (x) (foo x))) (macro-function 'foo) (compiler-macro-function 'foo) (symbol-function 'foo) (fdefinition 'foo) (foo 1) foo)\n",
         dialect: Dialect::CommonLisp,
         from: "foo",
         to: "bar",
         definitions: 1,
         calls: 2,
         changed: true,
-        rewritten_contains: ["(labels ((bar (x) (bar x))) (symbol-function 'foo) (fdefinition 'foo) (bar 1) foo)"]
+        rewritten_contains: ["(labels ((bar (x) (bar x))) (macro-function 'foo) (compiler-macro-function 'foo) (symbol-function 'foo) (fdefinition 'foo) (bar 1) foo)"]
     );
 }
--- tests/cli/rename/local_function/accessors.rs
+++ tests/cli/rename/local_function/accessors.rs
@@ -5,10 +5,10 @@ fn cli_writes_flet_rename_without_touching_global_function_cell_accessors() {
     assert_cli_local_function_write(
         "rename-local-function-global-accessors-flet-write",
         "lisp",
-        "(flet ((old-name (x) x)) (symbol-function 'old-name) (fdefinition 'old-name) (old-name 1) old-name)\n",
+        "(flet ((old-name (x) x)) (macro-function 'old-name) (compiler-macro-function 'old-name) (symbol-function 'old-name) (fdefinition 'old-name) (old-name 1) old-name)\n",
         1,
         1,
-        "(flet ((new-name (x) x)) (symbol-function 'old-name) (fdefinition 'old-name) (new-name 1) old-name)\n",
+        "(flet ((new-name (x) x)) (macro-function 'old-name) (compiler-macro-function 'old-name) (symbol-function 'old-name) (fdefinition 'old-name) (new-name 1) old-name)\n",
     );
 }
 
@@ -17,9 +17,9 @@ fn cli_writes_labels_rename_without_touching_global_function_cell_accessors() {
     assert_cli_local_function_write(
         "rename-local-function-global-accessors-labels-write",
         "lisp",
-        "(labels ((old-name (x) (old-name x))) (symbol-function 'old-name) (fdefinition 'old-name) (old-name 1) old-name)\n",
+        "(labels ((old-name (x) (old-name x))) (macro-function 'old-name) (compiler-macro-function 'old-name) (symbol-function 'old-name) (fdefinition 'old-name) (old-name 1) old-name)\n",
         1,
         2,
-        "(labels ((new-name (x) (new-name x))) (symbol-function 'old-name) (fdefinition 'old-name) (new-name 1) old-name)\n",
+        "(labels ((new-name (x) (new-name x))) (macro-function 'old-name) (compiler-macro-function 'old-name) (symbol-function 'old-name) (fdefinition 'old-name) (new-name 1) old-name)\n",
     );
 }
