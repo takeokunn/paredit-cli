d1e1e5e89aff80e9321100c5737a743220523b18
takeokunn <bararararatty@gmail.com>
2026-07-11 21:22:53 +0900

    test: add symbol macro plan coverage

--- tests/cli/rename/function/plan_symbol_macro_scope.rs
+++ tests/cli/rename/function/plan_symbol_macro_scope.rs
@@ -83,6 +83,67 @@ fn cli_plans_function_rename_inside_symbol_macrolet_expansion_forms() {
 }
 
 #[test]
+fn cli_plans_function_rename_through_qualified_symbol_macrolet_forms() {
+    assert_plan_case(PlanCase {
+        fixture_name: "rename-function-plan-qualified-symbol-macrolet-shadowing",
+        from: "helper",
+        to: "renamed",
+        input_files: &[FixtureFile {
+            path: "core.lisp",
+            contents: "(cl:defun helper (x) x)\n\
+(defun caller ()\n\
+  (cl:symbol-macrolet ((helper other))\n\
+    helper\n\
+    #'helper\n\
+    (function helper)\n\
+    (helper 1))\n\
+  (cl-user:symbol-macrolet ((helper other))\n\
+    helper\n\
+    #'helper\n\
+    (function helper)\n\
+    (helper 2))\n\
+  (helper 3)\n\
+  #'helper\n\
+  (function helper)\n\
+  (helper 4))\n",
+        }],
+        stdout_needles: &[
+            "\"definitionCount\": 1",
+            "\"callCount\": 10",
+            "\"path\": \"0.1\"",
+            "\"path\": \"1.3.3\"",
+            "\"path\": \"1.4.3\"",
+            "\"path\": \"1.8.0\"",
+            "\"rewritten\": \"(cl:defun renamed (x) x)",
+            "(cl:symbol-macrolet ((helper other))",
+            "(cl-user:symbol-macrolet ((helper other))",
+            "#'renamed",
+            "(function renamed)",
+            "(renamed 4))\\n\"",
+        ],
+        unchanged_files: &[FixtureFile {
+            path: "core.lisp",
+            contents: "(cl:defun helper (x) x)\n\
+(defun caller ()\n\
+  (cl:symbol-macrolet ((helper other))\n\
+    helper\n\
+    #'helper\n\
+    (function helper)\n\
+    (helper 1))\n\
+  (cl-user:symbol-macrolet ((helper other))\n\
+    helper\n\
+    #'helper\n\
+    (function helper)\n\
+    (helper 2))\n\
+  (helper 3)\n\
+  #'helper\n\
+  (function helper)\n\
+  (helper 4))\n",
+        }],
+    });
+}
+
+#[test]
 fn cli_plans_function_references_inside_reader_quoted_lambda_bodies_only() {
     assert_plan_case(PlanCase {
         fixture_name: "rename-function-plan-reader-quoted-lambda-symbol-macrolet",
