231eba41ba4b839ee010a9feff3289a5aaa9dd08
takeokunn <bararararatty@gmail.com>
2026-07-13 05:41:08 +0900

    refactor: move similarity report into domain

--- src/application/usecase/similarity_report/mod.rs
+++ src/application/usecase/similarity_report/mod.rs
@@ -1,14 +1,2 @@
-mod collect;
-mod reports;
-mod types;
-
-pub use collect::collect_similarity_candidates;
-pub use reports::{build_similarity_pairs, build_similarity_pairs_with_omissions};
-pub use types::{
-    SimilarityCandidate, SimilarityComparisonScope, SimilarityFormReport, SimilarityFormScope,
-    SimilarityOverlapPolicy, SimilarityPairReport, SimilarityReport, SimilarityReportOptions,
-    SimilarityReportOptionsError, SimilarityReportSummary,
-};
-
-#[cfg(test)]
-mod tests;
+//! Backwards-compatible application facade for similarity analysis.
+pub use crate::domain::similarity_report::*;
